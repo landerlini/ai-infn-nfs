f652cbdbc98e656fd0517c362032d5757bd01068
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2025-11-14 19:38:59 +0100

    BasicAuth for NFS administration

--- docker/apiserver.py
+++ docker/apiserver.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
 app = FastAPI()
+security = HTTPBasic()
+
+USERNAME = os.environ.get("HTTP_USERNAME")
+PASSWORD = os.environ.get("HTTP_PASSWORD")
+
+if not USERNAME:
+    raise ValueError("Invalid HTTP_USERNAME for administration tasks")
+
+if not PASSWORD:
+    raise ValueError("Invalid HTTP_PASSWORD for administration tasks")
+
+
+def authenticated_user(credentials: HTTPBasicCredentials = Depends(security)) -> str:
+    correct_username = secrets.compare_digest(credentials.username, USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, PASSWORD)
+
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
+async def root(user: str = Depends(authenticated_user)):
+    return {"message": f"Hello, {user}!"}
